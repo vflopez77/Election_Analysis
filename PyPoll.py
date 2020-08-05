32aaa6d206792d0cf137c8b828b16fc8e77e25de
vflopez77 <68453606+vflopez77@users.noreply.github.com>
2020-08-05 11:50:55 -0400

    Adding PyPoll.py file.

--- PyPoll.py
+++ PyPoll.py
@@ -14,19 +14,31 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-    # To do: perform analysis
-    print(election_data)
+    # To do: read and analyse data here
+    #print(election_data)
+    # Read the file object with the reader function
+    file_reader = csv.reader(election_data)
+
+    # Read and print the header row
+    headers = next(file_reader)
+    print(headers)
+
+    # Print each row in the CSV file
+    # for row in file_reader:
+    #     print(row)
+
 
 # Close the file
 #election_data.close()
 
-# Create a filename variable to a direct or indirect path th the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
-# Using the with statement open the file as a text file.
-with open(file_to_save, "w") as txt_file:
+# # Writing to Analysis text file
+# # Create a filename variable to a direct or indirect path the the file.
+# file_to_save = os.path.join("analysis", "election_analysis.txt")
+# # Using the with statement open the file as a text file.
+# with open(file_to_save, "w") as txt_file:
 
-    # Write the file header
-    txt_file.write("Counties in the Election\n")
-    txt_file.write("_________________________\n")
-    # Write three counties to the file.
-    txt_file.write("Arapahoe\nDenver\nJefferson")
+#     # Write the file header
+#     txt_file.write("Counties in the Election\n")
+#     txt_file.write("_________________________\n")
+#     # Write three counties to the file.
+#     txt_file.write("Arapahoe\nDenver\nJefferson")
