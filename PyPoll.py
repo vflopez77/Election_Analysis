9c40481c93e5655f154ff5983c6db7c79536a34c
vflopez77 <68453606+vflopez77@users.noreply.github.com>
2020-08-05 14:34:11 -0400

    Finishing vote counts

--- PyPoll.py
+++ PyPoll.py
@@ -22,6 +22,12 @@ candidate_options = []
 # Declare an empty candidate votes dictionary
 candidate_votes = {}
 
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -65,7 +71,28 @@ for candidate_name in candidate_votes:
     # Calculate the percentage of votes
     vote_percentage = float(votes) / float(total_votes) * 100
     # Print the candidate name and percentage of votes
-    print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+    #print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+
+    # Determine winning vote and candidate
+    # Determine if the votes are greater than the winning count
+    if votes > winning_count and vote_percentage > winning_percentage:
+        # If true set the winning count to votes and the winning percentage to vote_percentage
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # Set winning_candidate equal to the candidate's name
+        winning_candidate = candidate_name 
+
+    # Print out each candidate's name, vote count, and percentage of votes to the terminal
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+# Print out winning candidate summary
+winning_candidate_summary = (
+    f"---------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"---------------------------\n")
+print(winning_candidate_summary)
 
 # Print the candidate list
 #print(candidate_options)
