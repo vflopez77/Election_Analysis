38a313a66a6204b47092f001246c074fa3066504
vflopez77 <68453606+vflopez77@users.noreply.github.com>
2020-08-05 12:04:17 -0400

    Updating PyPoll.py file.

--- PyPoll.py
+++ PyPoll.py
@@ -24,8 +24,8 @@ with open(file_to_load) as election_data:
     print(headers)
 
     # Print each row in the CSV file
-    # for row in file_reader:
-    #     print(row)
+    for row in file_reader:
+        print(row)
 
 
 # Close the file
