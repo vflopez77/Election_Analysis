4bfaeec82615035226b693d5f13fc26ffe480711
vflopez77 <68453606+vflopez77@users.noreply.github.com>
2020-08-05 12:54:43 -0400

    Adding Total Vote Counter

--- PyPoll.py
+++ PyPoll.py
@@ -10,6 +10,11 @@ import os
 
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to load the file from a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# 1. Initialize a total vote counter
+total_votes = 0
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -19,21 +24,23 @@ with open(file_to_load) as election_data:
     # Read the file object with the reader function
     file_reader = csv.reader(election_data)
 
-    # Read and print the header row
+    # Read the header row
     headers = next(file_reader)
-    print(headers)
+    #print(headers)
 
-    # Print each row in the CSV file
+    # Read each row in the CSV file
     for row in file_reader:
-        print(row)
+        #print(row)
+        # 2. Add to the total vote count
+        total_votes += 1
 
+# 3. Print the total votes
+print(total_votes)
 
 # Close the file
 #election_data.close()
 
 # # Writing to Analysis text file
-# # Create a filename variable to a direct or indirect path the the file.
-# file_to_save = os.path.join("analysis", "election_analysis.txt")
 # # Using the with statement open the file as a text file.
 # with open(file_to_save, "w") as txt_file:
 
