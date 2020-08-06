9d4f93c9573e8bd2a3e811ce011285635f8706ba
vflopez77 <68453606+vflopez77@users.noreply.github.com>
2020-08-05 21:32:19 -0400

    Updating PyPoll_Challenge.py with county data

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -88,7 +88,8 @@ with open(file_to_save, "w") as txt_file:
     txt_file.write(election_results)
 
     # 6a: Write a repetition statement to get the county from the county dictionary.
-    print("County Votes:")
+    print("County Votes")
+    txt_file.write("County Votes:\n")
     for county in counties:
         # 6b: Retrieve the county vote count.
         county_vote = county_votes.get(county)
@@ -101,7 +102,7 @@ with open(file_to_save, "w") as txt_file:
         print(county_results)
 
          # 6e: Save the county votes to a text file.
-        txt_file.write(county_results)
+        txt_file.write(county_results + "\n")
 
          # 6f: Write a decision statement to determine the winning county and get its vote count.
         if (county_vote > largest_county_count) and (county_percentage > largest_county_percentage):
@@ -115,7 +116,10 @@ with open(file_to_save, "w") as txt_file:
     print("-------------------------\n")
     
     # 8: Save the county with the largest turnout to a text file.
-
+    txt_file.write("\n-------------------------\n")
+    txt_file.write(f"Largest County Turnout: {largest_county}\n")
+    txt_file.write("-------------------------\n")
+    
 
     # Save the final candidate vote count to the text file.
     for candidate_name in candidate_votes:
